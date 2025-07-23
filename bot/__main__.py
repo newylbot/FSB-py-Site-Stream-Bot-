1b920774ad3b73a23c6bcac5e089c72a6436c0a0
Rak-X <mimik1sudosu@gmail.com>
2025-07-23 15:44:22 +0530

    Fix circular import and add plugin package

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 from bot import TelegramBot
-from bot.server import server
+from bot.server import server, init_app
 
 if __name__ == '__main__':
+    init_app()
     TelegramBot.loop.create_task(server.serve())
     TelegramBot.run()
