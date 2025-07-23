199c3ecea1db377a1dd5d100a3c73f10b2ea751b
Rak-X <mimik1sudosu@gmail.com>
2025-07-24 01:36:03 +0530

    Improve caption parsing

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -2,7 +2,7 @@ from hydrogram.types import CallbackQuery
 from bot import TelegramBot
 from bot.modules.decorators import verify_user
 from bot.modules.static import *
-from bot.modules.telegram import get_message
+from bot.modules.telegram import get_message, parse_caption_meta
 
 @TelegramBot.on_callback_query()
 @verify_user
@@ -10,19 +10,19 @@ async def manage_callback(bot, q: CallbackQuery):
     query = q.data
 
     if query.startswith('rm_'):
-        sq = query.split('_')
+        parts = query.split('_')
 
-        if len(sq) != 3:
+        if len(parts) != 3:
             return await q.answer(InvalidQueryText, show_alert=True)
 
-        message = await get_message(int(sq[1]))
+        message = await get_message(int(parts[1]))
 
         if not message:
             return await q.answer(MessageNotExist, show_alert=True)
         
-        sc = message.caption.split('/')
+        stored_code, user_id = parse_caption_meta(message.caption)
 
-        if q.from_user.id != int(sc[1]) or sq[2] != sc[0]:
+        if q.from_user.id != user_id or parts[2] != stored_code:
             return await q.answer(InvalidQueryText, show_alert=True)
         
         await message.delete()
